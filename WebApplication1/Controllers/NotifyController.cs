078e4834862a9b05737dceba6719bfbdba198b07
Pandit <703177712@genpact.com>
2019-06-24 01:31:07 +0530

    initial
    sfd

---
+++ WebApplication1/Controllers/NotifyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class NotifyController : ApiController
+    {
+        private static readonly IHubContext context;
+
+        static NotifyController()
+        {
+            context = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+        }
+        [HttpPost, Route("notify")]
+        public IHttpActionResult NotifyAsync(SignalRNotification notification)
+        {
+            context.Clients.Group(notification.GroupId).ShowNotification(notification.SignalRMessage);
+            return Ok();
+        }
+    }
+}
